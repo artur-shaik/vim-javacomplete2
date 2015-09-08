a45147a7ce1540102eef82b15e590d1f5564bb29
Artur Shaik <ashaihullin@gmail.com>
2015-09-08 20:46:44 +0600

    Fix auto add annotations and exceptions.

--- libs/javavi/src/main/java/kg/ash/javavi/readers/source/ClassNamesFetcher.java
+++ libs/javavi/src/main/java/kg/ash/javavi/readers/source/ClassNamesFetcher.java
@@ -2,13 +2,20 @@ package kg.ash.javavi.readers.source;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.TypeParameter;
+import com.github.javaparser.ast.body.AnnotationDeclaration;
+import com.github.javaparser.ast.body.AnnotationMemberDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.AnnotationExpr;
 import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.FieldAccessExpr;
+import com.github.javaparser.ast.expr.MemberValuePair;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.expr.MethodReferenceExpr;
+import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.expr.ObjectCreationExpr;
 import com.github.javaparser.ast.expr.QualifiedNameExpr;
 import com.github.javaparser.ast.expr.TypeExpr;
@@ -20,6 +27,7 @@ import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.ast.visitor.DumpVisitor;
 import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.sun.javafx.css.Declaration;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -36,10 +44,57 @@ public class ClassNamesFetcher {
     }
 
     public Set<String> getNames() {
+        ClassTypeVisitor classVisitor = new ClassTypeVisitor();
+        classVisitor.visit(compilationUnit, null);
+
         TypesVisitor visitor = new TypesVisitor();
         visitor.visit(compilationUnit, null);
 
+        AnnotationsVisitor annotationsVisitor = new AnnotationsVisitor();
+        annotationsVisitor.visit(compilationUnit, null);
+
         return resultList;
+    }
+
+    private class ClassTypeVisitor extends VoidVisitorAdapter<Object> {
+
+        @Override
+        public void visit(ClassOrInterfaceDeclaration type, Object arg) {
+            if (type.getAnnotations() != null) {
+                for (AnnotationExpr expr : type.getAnnotations()) {
+                    resultList.add(expr.getName().getName());
+                }
+            }
+        }
+
+    }
+
+    private class AnnotationsVisitor extends VoidVisitorAdapter<Object> {
+
+        private void addAnnotations(List<AnnotationExpr> annotations) {
+            if (annotations != null) {
+                for (AnnotationExpr expr : annotations) {
+                    resultList.add(expr.getName().getName());
+                }
+            }
+        }
+
+        @Override
+        public void visit(FieldDeclaration type, Object arg) {
+            addAnnotations(type.getAnnotations());
+        }
+
+        @Override
+        public void visit(MethodDeclaration type, Object arg) {
+            addAnnotations(type.getAnnotations());
+
+            if (type.getThrows() != null) {
+                for (NameExpr expr : type.getThrows()) {
+                    resultList.add(expr.getName());
+                }
+            }
+        }
+
     }
 
     private class TypesVisitor extends VoidVisitorAdapter<Object>{
