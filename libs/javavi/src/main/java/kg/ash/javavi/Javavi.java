27fe128e3e818732dd3957608446d506f2f27788
Artur Shaik <ashaihullin@gmail.com>
2015-12-26 19:33:37 +0600

    Set intermediate version number

--- libs/javavi/src/main/java/kg/ash/javavi/Javavi.java
+++ libs/javavi/src/main/java/kg/ash/javavi/Javavi.java
@@ -8,7 +8,7 @@ import kg.ash.javavi.clazz.SourceClass;
 
 public class Javavi {
 
-    static final String VERSION	= "2.3.4";
+    static final String VERSION	= "2.3.4.1";
 
     public static String NEWLINE = "";
 
