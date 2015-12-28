47fbf24d194a9403fda1c30c631f8ce81c3d8953
Artur Shaik <ashaihullin@gmail.com>
2015-12-28 20:31:13 +0600

    Test directories

--- libs/javavi/src/test/java/kg/ash/javavi/DaemonTest.java
+++ libs/javavi/src/test/java/kg/ash/javavi/DaemonTest.java
@@ -5,9 +5,10 @@ import org.junit.Test;
 
 public class DaemonTest {
 
+    private Daemon daemon = new Daemon(0, -1);
+
     @Test
     public void testParseLine() {
-        Daemon daemon = new Daemon(0, -1);
         Assert.assertArrayEquals(new String[]{"-v"}, daemon.parseRequest("-v"));
         Assert.assertArrayEquals(new String[]{"-E", "java.util.List"}, daemon.parseRequest("-E \"java.util.List\""));
         Assert.assertArrayEquals(new String[]{"-E", "java.util.List"}, daemon.parseRequest("-E java.util.List"));
@@ -15,6 +16,15 @@ public class DaemonTest {
         Assert.assertArrayEquals(new String[0], daemon.parseRequest(""));
         Assert.assertArrayEquals(new String[]{"-E", ""}, daemon.parseRequest("-E \"\""));
         Assert.assertEquals("\\n", daemon.parseRequest("\"\\\\n\"")[0]);
+    }
+
+    @Test
+    public void testParseEmptyValue() {
+        Assert.assertEquals(new String[]{"-sources", ""}, daemon.parseRequest("-sources \"\""));
+    }
+
+    @Test
+    public void testWindowsDirectoryValue() {
         Assert.assertEquals(new String[]{"-base", "C:\\Documents and Settings\\directory\\"}, daemon.parseRequest("-base \"C:\\\\Documents and Settings\\\\directory\\\\\""));
     }
 
