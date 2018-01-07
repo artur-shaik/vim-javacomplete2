1d8402b8c4c1cb4207e86f390711bbe9d6693d2d
Artur Shaik <ashaihullin@gmail.com>
2018-01-07 17:33:23 +0600

    Bump version to 2.3.8

--- libs/javavi/src/main/java/kg/ash/javavi/Javavi.java
+++ libs/javavi/src/main/java/kg/ash/javavi/Javavi.java
@@ -13,7 +13,7 @@ import kg.ash.javavi.searchers.ClasspathCollector;
 
 public class Javavi {
 
-    public static final String VERSION	= "2.3.7.3";
+    public static final String VERSION	= "2.3.8";
 
     public static String NEWLINE = "";
 
