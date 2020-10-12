9a09d8059cbcb2539e9697ee07322fa0cc15ebd0
Artur Shaik <artur@shaik.link>
2020-10-12 11:03:36 +0600

    show error from deoplete source

--- rplugin/python3/deoplete/sources/javacomplete2.py
+++ rplugin/python3/deoplete/sources/javacomplete2.py
@@ -1,3 +1,4 @@
+import pynvim
 from deoplete.base.source import Base
 
 class Source(Base):
@@ -18,5 +19,9 @@ class Source(Base):
                              1, '', 0)
 
     def gather_candidates(self, context):
-        return self.vim.call('javacomplete#complete#complete#Complete',
-                             0, context['complete_str'], 0)
+        try:
+            return self.vim.call('javacomplete#complete#complete#Complete',
+                                 0, context['complete_str'], 0)
+        except pynvim.api.common.NvimError as er:
+            print(er)
+            raise er
